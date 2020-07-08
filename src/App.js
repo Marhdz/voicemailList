7519d60c3955428c9a05ee22ce9c7db3359c07e6
Marhdz <marhdz0@gmail.com>
2020-07-07 23:12:36 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { forwardRef } from 'react';
+import Grid from '@material-ui/core/Grid'
+import MaterialTable from "material-table";
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import axios from 'axios'
+import Alert from '@material-ui/lab/Alert';
 
-function App() {
+const tableIcons = {
+  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+  // ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+  // ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
+
+const api = axios.create({
+  baseURL: `https://api-vm.herokuapp.com`
+})
+
+
+
+
+function App(props) {
+
+const {useState} = React
+
+  const [columns, setColumns] = useState([
+
+    {title: "call_id", field: "call_id", hidden: true},
+    {title: "Status", field: "folder"},
+    {title: "From", field: "from"},
+    {title: "To", field: "to"},
+    {title: "Duration", field: "length"},
+    {title: "Caller_id", field: "caller_id_number"},
+    {title: "Time", field:"timestamp"}
+  ])
+
+
+  const [data, setData] = useState([]); //table data
+
+  //for error handling
+  const [iserror, setIserror] = useState(false)
+  const [errorMessages, setErrorMessages] = useState([])
+
+  useEffect(() => {
+    api.get("/users")
+        .then(res => {
+            setData(res.data.data)
+         })
+         .catch(error=>{
+             console.log("Error")
+         })
+  }, [])
+
+  const handleRowUpdate = (newData, oldData, resolve) => {
+    //validation
+    let errorList = []
+
+    if(errorList.length < 1){
+      api.patch("/users/"+newData.call_id, newData)
+      .then(res => {
+        const dataUpdate = [...data];
+        const index = oldData.tableData.call_id;
+        dataUpdate[index] = newData;
+        setData([...dataUpdate]);
+        resolve()
+        setIserror(false)
+        setErrorMessages([])
+      })
+      .catch(error => {
+        setErrorMessages(["Update failed! Server error"])
+        setIserror(true)
+        resolve()
+      })
+    }else{
+      setErrorMessages(errorList)
+      setIserror(true)
+      resolve()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Grid container container spacing={1} >
+          <Grid item xs={3}></Grid>
+          <Grid item xs={6}>
+          <div>
+            {iserror &&
+              <Alert severity="error">
+                  {errorMessages.map((msg, i) => {
+                      return <div key={i}>{msg}</div>
+                  })}
+              </Alert>
+            }
+          </div>
+            <MaterialTable
+              title="Voicemails"
+              columns={columns}
+              data={data}
+              icons={tableIcons}
+              editable={{
+                onRowUpdate: (newData, oldData) =>
+                  new Promise((resolve) => {
+                      handleRowUpdate(newData, oldData, resolve);
+                  }),
+              }}
+              options={{
+                search: false
+              }}
+            />
+          </Grid>
+          <Grid item xs={3}></Grid>
+        </Grid>
     </div>
   );
 }
